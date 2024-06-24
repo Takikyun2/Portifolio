0ef95aa4000e28351c6a0243ff904bcfebcf4159
Takikyun2 <darksd016@gmail.com>
2024-06-23 21:55:40 -0300

    update: finish main parts on desktop ver.

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/* seções do site */
+
+const aboutMeContainer = document.querySelector('.aboutMeContainer');
+const projectsContainer = document.querySelector('.projectsContainer');
+const contactContainer = document.querySelector('.contactContainer');
+
+/* botoes do move bar */
+const aboutMePage = document.querySelector('.aboutMePage');
+const projectsPage = document.querySelector('.projectsPage');
+const contactPage = document.querySelector('.contactPage');
+
+
+aboutMePage.addEventListener('click', () => {
+  aboutMeContainer.style.display = "flex";
+  projectsContainer.style.display = "none";
+  contactContainer.style.display = "none";
+
+})
+
+projectsPage.addEventListener('click', () => {
+  aboutMeContainer.style.display = "none";
+  projectsContainer.style.display = "flex";
+  contactContainer.style.display = "none";
+
+})
+
+contactPage.addEventListener('click', () => {
+  aboutMeContainer.style.display = "none";
+  projectsContainer.style.display = "none";
+  contactContainer.style.display = "flex";
+
+})
